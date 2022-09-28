cda5faa8a0498776dbea76ebf3d34a23326b2d50
NP-desktop <polt.gun@gmail.com>
2022-09-28 20:36:32 +0200

    clean import

--- src/main/java/org/poltou/opening/ChessNode.java
+++ src/main/java/org/poltou/opening/ChessNode.java
@@ -3,13 +3,8 @@ package org.poltou.opening;
 import java.util.Map;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
